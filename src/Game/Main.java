9bac998f24bd8a041c50d28caeed7b89a3193ed0
Giovanni <Giovanni@130.215.175.54>
2016-04-10 14:03:51 -0400

    added Application

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -3,6 +3,6 @@ package Game;
 public class Main {
 	public static void main (String[] args) {
 	  //change
-		System.out.println("Achilles is Number One");
+		System.out.println("Paul sucks");
 	}
 }
