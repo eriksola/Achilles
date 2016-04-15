32002beb86b013af2eef1a1d1c5b45d9378740cf
Barrett Wolfson <bwolfson@wpi.edu>
2016-04-15 11:14:46 -0400

    changed Piece structure to 6 Coordinates, and added BullPen functions on Piece

--- src/Game/BullPen.java
+++ src/Game/BullPen.java
@@ -3,9 +3,53 @@ package Game;
 public class BullPen {
 	
 	Piece[] pieces;
+	Piece selectedPiece;
 	
 	public BullPen(Piece[] p){
 		this.pieces = p;
-		
+	}
+	
+	/**
+	 * rotate the selected piece ninety degrees counterclockwise
+	 */
+	public void rotateLeft(){
+		for(Coordinate c: selectedPiece.coords){
+			int oldX = c.x;
+			int oldY = c.y;
+			c.x = (-1) * oldY;
+			c.y = (1) * oldX;
+		}
+	}
+	
+	/**
+	 * rotate the selected piece ninety degrees clockwise
+	 */
+	public void rotateRight(){
+		for(Coordinate c: selectedPiece.coords){
+			int oldX = c.x;
+			int oldY = c.y;
+			c.x = (1) * oldY;
+			c.y = (1) * oldX;
+		}
+	}
+	
+	/**
+	 * flip the selected piece over it's x-axis
+	 */
+	public void verticalFlip(){
+		for(Coordinate c: selectedPiece.coords){
+			int oldY = c.y;
+			c.y = (-1) * oldY;
+		}
+	}
+	
+	/**
+	 * flip the selected piece over it's y-axis
+	 */
+	public void horizontalFlip(){
+		for(Coordinate c: selectedPiece.coords){
+			int oldX = c.x;
+			c.x = (-1) * oldX;
+		}
 	}
 }
