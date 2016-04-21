9ff3f70784aff0d218a185ff3acdd99c2839347e
DIRECTIONNZ <ZhangNan@autoreg-713833.wifi.wpi.edu>
2016-04-21 14:02:57 -0400

    addPiece method on Board

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -3,10 +3,19 @@ package Game;
 //testing git
 public class Board {
 
-	Tile[] tiles;
+	Tile[][] tiles;
 		
-	Board(Tile[] t){
+	Board(Tile[][] t){
 		
 		this.tiles = t;
+	}
+	
+	public Board addPiece(int row, int column, Piece piece){
+		this.tiles[row][column] = new Tile(true, row, column);
+		for (int i =1; i<6; i++){
+			Coordinate c =piece.getCoordinates()[i];
+			this.tiles[row+c.x][column+c.y] = new Tile(true, row+c.x, column+c.y);
+		}
+		return this;
 	}
 }
