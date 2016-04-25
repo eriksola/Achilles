c65d3d378346110d1ca1114ff4092c565a98a142
Barrett Wolfson <bwolfson@wpi.edu>
2016-04-24 21:27:07 -0400

    bullpen controllers

---
+++ src/Controller/RotateController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Boundary.KabaSuji;
+import Boundary.PieceView;
+import Game.Piece;
+
+public class RotateController implements ActionListener{
+
+	KabaSuji view;
+	
+	public RotateController(KabaSuji view){
+		this.view = view;
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		//check if selected piece is in bullpen (otherwise moves not allowed)
+		PieceView selected = view.getSelectedPiece();
+		if(selected != null){
+			Piece p = selected.getP();
+			p.rotateRight();
+			selected.drawSelected();
+		}
+	}
+	
+}
---
+++ src/Controller/VflipController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Boundary.KabaSuji;
+import Boundary.PieceView;
+import Game.Piece;
+
+public class VflipController implements ActionListener{
+
+	KabaSuji view;
+	
+	public VflipController(KabaSuji view){
+		this.view = view;
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		//check if selected piece is in bullpen (otherwise moves not allowed)
+		PieceView selected = view.getSelectedPiece();
+		Piece p = selected.getP();
+		p.verticalFlip();
+		selected.drawSelected();
+	}
+	
+}
