b7080d623a3be7cb2364083ded671e91186ccd35
bwolfson <bwolfson@KH207-13.admin.wpi.edu>
2016-04-24 22:11:33 -0400

    update

---
+++ src/Main/test.java
@@ -0,0 +1,22 @@
+package Main;
+
+import javax.swing.JFrame;
+
+import Boundary.BullPenView;
+import Boundary.KabaSuji;
+import Game.BullPen;
+import Game.Stock;
+
+public class test {
+
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		Stock s = new Stock();
+		BullPen bp = new BullPen(s.getRandomPiecesForPen());
+		BullPenView bpv = new BullPenView(frame, bp, new KabaSuji());
+		frame.getContentPane().add(bpv);
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+	}
+}
