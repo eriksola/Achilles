adcff3c13f95b948e2ea2865b28f20fcff773e36
bwolfson <bwolfson@KH207-13.admin.wpi.edu>
2016-04-26 01:40:52 -0400

    update BullPenController

---
+++ src/Controller/BullPenController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+
+import Boundary.BullPenView;
+import Boundary.KabaSuji;
+import Boundary.LevelBuilderPuzzlePanel;
+import Boundary.PieceView;
+import Game.Board;
+import Game.Piece;
+
+public class BullPenController extends java.awt.event.MouseAdapter{
+	
+	KabaSuji view;
+	BullPenView bpv;
+	
+	public BullPenController(KabaSuji view, BullPenView bpv){
+		this.view = view;
+		this.bpv = bpv;
+	}
+	
+	public void mouseClicked(MouseEvent me){
+		Point p = me.getPoint();
+		System.out.println("mouse clicked from bullpen");
+		if(view.getSelectedPiece() != null){
+			bpv.addPiece(new PieceView(view.getSelectedPiece().getP(), view));
+			((LevelBuilderPuzzlePanel)view).getScrollPane().setViewportView(bpv);
+		}
+	}
+
+}
