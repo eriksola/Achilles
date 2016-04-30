4974ace898864db957ab8855333e4e91cbc21794
bwolfson <bwolfson@KH207-09.admin.wpi.edu>
2016-04-30 00:28:51 -0400

    more testing - trying to figure out how to test GUI objects

--- Test/test/TestEntities.java
+++ Test/test/TestEntities.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 
 import Boundary.Both.KabaSuji;
@@ -7,13 +9,15 @@ import Boundary.Both.PieceView;
 import Boundary.Builder.LevelBuilderFrame;
 import Boundary.Builder.LevelBuilderPuzzlePanel;
 import Game.Board;
+import Game.BullPen;
 import Game.Coordinate;
 import Game.Piece;
 import Game.Stock;
 import Game.Tile;
 import junit.framework.TestCase;
 
-public class TestBoard extends TestCase {
+public class TestEntities extends TestCase {
+	
 	Tile[][] t = new Tile[6][6];
 	Board board = new Board(t);
 	Stock s = new Stock();
@@ -26,8 +30,14 @@ public class TestBoard extends TestCase {
 	Coordinate c4 = new Coordinate(-1,0);
 	Coordinate c5 = new Coordinate(0,-1);
 	Coordinate c6 = new Coordinate(0,-2);
+	Coordinate c7 = new Coordinate(-2,0);
+	Coordinate c8 = new Coordinate(2,0);
 
 	Piece p1 = new Piece(new Coordinate[]{c1,c2,c3,c4,c5,c6},0);
+	Piece p2 = new Piece(new Coordinate[]{c1,c2,c3,c4,c5,c7},0);
+	Piece p3 = new Piece(new Coordinate[]{c1,c2,c3,c4,c5,c8},0);
+	Piece p4 = new Piece(new Coordinate[]{c1,c2,c3,c4,c5,c6},0);
+	
 	/*
 	 *    #  
 	 *  # # #
@@ -43,6 +53,8 @@ public class TestBoard extends TestCase {
 			}
 		}
 	}
+	
+	/////////////////////////////Board Tests////////////////////////////////
 
 	public void testIsValid(){
 		assertFalse(board.isValid(7, 6));
@@ -70,5 +82,46 @@ public class TestBoard extends TestCase {
 		assertTrue(board.selectPiece(1, 1, pv));
 	}
 	
+	//////////////////////////////Piece Tests////////////////////////////////
+	
+	public void testRotation(){
+		Coordinate[] originals = p1.getCoordinates();
+		p1.rotateRight();
+		p1.rotateLeft();
+		for (int i = 0; i < originals.length; i++) {
+			assertEquals(originals[i].x, p1.getCoordinates()[i].x);
+			assertEquals(originals[i].y, p1.getCoordinates()[i].y);
+		}
+		
+	}
+	
+	public void testFlips(){
+		String pString = p1.toString();
+		Coordinate[] originals = p1.getCoordinates();
+		p1.verticalFlip();
+		p1.verticalFlip();
+		p1.horizontalFlip();
+		p1.horizontalFlip();
+		for (int i = 0; i < originals.length; i++) {
+			assertEquals(originals[i].x, p1.getCoordinates()[i].x);
+			assertEquals(originals[i].y, p1.getCoordinates()[i].y);
+		}
+		
+	}
+	
+	///////////////////////////BullPen Tests///////////////////////////////
+
+	public void testGetSelectedPiece(){
+		ArrayList<Piece> a = new ArrayList<Piece>();
+		a.add(p1);
+		a.add(p2);
+		
+		BullPen b1 = new BullPen(a);
+		b1.addPiece(p3);
+		
+		b1.setSelectedPiece(p2);
+		assertTrue(b1.selectedPiece == p2);
+		assertEquals(b1.getSelectedPiece(),p2);
+	}
 	
 }
