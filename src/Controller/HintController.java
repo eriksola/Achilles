de2029220da17fadf5c9c61447f7deea72ae7be2
Barrett Wolfson <bwolfson@wpi.edu>
2016-05-01 03:11:28 -0400

    change HashMap from having PieceView to having Piece. Release levels in Player still not working

---
+++ src/Controller/HintController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Boundary.Both.KabaSuji;
+import Boundary.Both.PieceView;
+import Game.Piece;
+
+public class HintController implements ActionListener{
+
+	KabaSuji view;
+	
+	public HintController(KabaSuji view){
+		this.view = view;
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		//check if selected piece is in bullpen (otherwise moves not allowed)
+		PieceView selected = view.getSelectedPiece();
+		view.getBoardView().getBoard().registerHintPiece(selected.getP());
+		view.getBoardView().draw();
+	}
+	
+}
